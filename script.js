181c16ad27dbb285129fb2b0fca47b7056b6ada3
Navin Jethwani <navinjethwani76@gmail.com>
2023-03-02 00:15:32 +0530

    adding classes and eraser snapping

--- script.js
+++ script.js
@@ -1,121 +1,195 @@
-for (let i=1;i<=editorCount;i++){
-    const threshold = 10;
-    let boardID = `board-${i}`;
-    console.log(boardID)
-    const newBoard = document.getElementById(boardID);
-    console.log(newBoard)
-    const board = newBoard.querySelector("svg");
-    const boardText = newBoard.getElementById("text");
-    const mode = newBoard.getElementById("mode-select");
-    const svgRect = newBoard.getBoundingClientRect();
-    
-    var isPainting = false;
-    var isErasing = false;
-    
-    board.addEventListener("mousedown", start)
-    board.addEventListener("mouseup", stop);
-    board.addEventListener("mousemove", draw);
-    mode.addEventListener('change', handleModeChange);
-    
-    function start(event){
-        if(mode.value === 'pencil'){
-            isPainting = true;
-            isErasing = false;
-            const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-            let x1 = event.clientX - svgRect.left;
-            let y1 = event.clientY - svgRect.top;
-            let x2 = event.clientX - svgRect.left;
-            let y2 = event.clientY - svgRect.top;
-            line.setAttribute('x1', x1);
-            line.setAttribute('y1', y1);
-            line.setAttribute('x2', x2);
-            line.setAttribute('y2', y2);
-    
-            const lines = document.querySelectorAll('line');
-            for(let i=0; i<lines.length; i++){
-                const x1Other = lines[i].getAttribute("x1");
-                const y1Other = lines[i].getAttribute("y1");
-                const x2Other = lines[i].getAttribute("x2");
-                const y2Other = lines[i].getAttribute("y2");
-                const distanceStart = Math.sqrt((x1 - x1Other) ** 2 + (y1 - y1Other) ** 2);
-                const distanceEnd = Math.sqrt((x1 - x2Other) ** 2 + (y1 - y2Other) ** 2);
-                if (distanceStart < threshold) {
-                    line.setAttribute("x1", x1Other);
-                    line.setAttribute("y1", y1Other);
-                }
-                if (distanceEnd < threshold) {
-                    line.setAttribute("x1", x2Other);
-                    line.setAttribute("y1", y2Other);
-                  }
+
+class Draw{
+
+
+    constructor(id){
+        this.element = document.getElementById(id);
+        this.shapes = [];
+        this.currentShape = null; 
+        this.mode = "pencil";
+
+        const selectTag = document.getElementById("mode-select");
+            selectTag.addEventListener("change", (event) => {
+                this.mode = event.target.value;
+        });
+
+        this.element.addEventListener("mousedown", (event) => {
+            if (this.mode === 'pencil') {
+              this.startShape(event.offsetX, event.offsetY);
+            } else if (this.mode === 'eraser') {
+              this.eraseShapes(event.offsetX, event.offsetY);
             }
-            board.appendChild(line);
+          });
+        this.element.addEventListener("mousemove", (event) => {
+          
+            if(this.mode === 'pencil'){
+              this.updateShape(event.offsetX, event.offsetY);
+            }
+            else if (this.mode === 'eraser'){
+              this.highlightLines(event.offsetX, event.offsetY);
+            }
+        });
+        this.element.addEventListener("mouseup", (event) => {
+            this.endShape();
+        });
+    }
+
+
+
+    highlightLines(x, y){
+
+      for (const shape of this.shapes) {
+        const element = shape.element;
+        const x1 = Number(element.getAttribute("x1"));
+        const y1 = Number(element.getAttribute("y1"));
+        const x2 = Number(element.getAttribute("x2"));
+        const y2 = Number(element.getAttribute("y2"));
+        const distance = this.getPerpendicularDistance(x, y, x1, y1, x2, y2);
+        if (distance <= 15) {
+            element.classList.add('highlight');
+            }
+        else{
+            element.classList.remove('highlight');
         }
-        else if(mode.value === 'eraser'){
-            isErasing = true;
-            isPainting = false;
-            const element = document.elementFromPoint(event.clientX, event.clientY);
-            if(element.tagName === 'line'){
-                element.remove();
-            }
+      }
+    }
+
+
+
+    getPerpendicularDistance(x, y, x1, y1, x2, y2) {
+      const slope = (y2 - y1) / (x2 - x1);
+      const yIntercept = y1 - slope * x1;
+      const perpendicularSlope = -1 / slope;
+      const midX = (x1 + x2) / 2;
+      const midY = (y1 + y2) / 2;
+      const perpendicularYIntercept = midY - perpendicularSlope * midX;
+      const intersectionX = (perpendicularYIntercept - yIntercept) / (slope - perpendicularSlope);
+      const intersectionY = slope * intersectionX + yIntercept;
+      const distance = Math.sqrt(Math.pow(x - intersectionX, 2) + Math.pow(y - intersectionY, 2));
+      return distance;
+    }
+
+
+
+    startShape(x, y) {
+        this.currentShape = new Shape(x, y);
+        this.element.appendChild(this.currentShape.element);
+    }
+
+
+
+    updateShape(x, y) {
+        if (this.currentShape !== null) {
+          this.currentShape.addPoint(x, y);
         }
     }
-    
-    function draw(event){
-        if(isPainting === false) return;
-        else if(isPainting === true){
-            boardText.setAttribute('style', 'display: none;');
-            const line = board.lastChild;
-            const x2 = event.clientX - svgRect.left;
-            const y2 = event.clientY - svgRect.top
-            line.setAttribute('x2', x2);
-            line.setAttribute('y2', y2);
-            const lines = document.querySelectorAll('line');
-            for(let i=0; i<lines.length; i++){
-                const x1Other = lines[i].getAttribute("x1");
-                const y1Other = lines[i].getAttribute("y1");
-                const x2Other = lines[i].getAttribute("x2");
-                const y2Other = lines[i].getAttribute("y2");
-                const distanceStart = Math.sqrt((x2 - x1Other) ** 2 + (y2 - y1Other) ** 2);
-                const distanceEnd = Math.sqrt((x2 - x2Other) ** 2 + (y2 - y2Other) ** 2);
-                if (distanceStart < threshold) {
-                    line.setAttribute("x2", x1Other);
-                    line.setAttribute("y2", y1Other);
-                }
-                if (distanceEnd < threshold) {
-                    line.setAttribute("x2", x2Other);
-                    line.setAttribute("y2", y2Other);
-                }
-    
-            }
+
+
+    endShape() {
+        if (this.currentShape !== null) {
+          this.shapes.push(this.currentShape);
+          this.currentShape = null;
         }
     }
-    
-    function stop(event){
-        isPainting = false;
-        isErasing = false;
-        const emptyLine = document.querySelectorAll("line");
-        for(let i=0; i<emptyLine.length; i++){
-            if(emptyLine[i].getTotalLength() === 0){
-                emptyLine[i].remove();
-            }
+
+
+    eraseShapes(x, y) {
+      const shapeElementsToRemove = [];
+      for (const shape of this.shapes) {
+        const element = shape.element;
+        const x1 = Number(element.getAttribute("x1"));
+        const y1 = Number(element.getAttribute("y1"));
+        const x2 = Number(element.getAttribute("x2"));
+        const y2 = Number(element.getAttribute("y2"));
+        const distance = this.getPerpendicularDistance(x, y, x1, y1, x2, y2);
+        console.log(distance);
+        if (distance <= 20) {
+          shapeElementsToRemove.push(shape.element);
+          console.log(shapeElementsToRemove);
         }
-        if(!board.hasChildNodes()){
-            boardText.setAttribute('style', '');
-        }
+      }
     
-        const element = document.elementFromPoint(event.clientX, event.clientY);
-            if(element.tagName === 'line'){
-                element.addEventListener('mouseenter', () => {
-                    element.classList.add('highlight');
-                });
-                element.addEventListener('mouseleave', () => {
-                    element.classList.remove('highlight');
-                });
-            }
+      for (const element of shapeElementsToRemove) {
+        element.remove();
+        const index = this.shapes.findIndex(shape => shape.element === element);
+        if (index !== -1) {
+          this.shapes.splice(index, 1);
+        }
+      }
     }
     
-    function handleModeChange() {
-        isDrawing = false;
-        isErasing = false;
-    }
+    
 }
+
+
+
+
+class Shape {
+
+
+
+    constructor(x, y) {
+        
+        this.element = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        this.element.setAttribute("x1", x);
+        this.element.setAttribute("y1", y);
+        this.element.setAttribute("x2", x);
+        this.element.setAttribute("y2", y);
+
+        this.boardText = document.getElementById('board-text');
+        const lines = document.querySelectorAll('line')
+        for(let i=0; i<lines.length; i++){  
+            const x1Other = lines[i].getAttribute("x1");
+            const y1Other = lines[i].getAttribute("y1");
+            const x2Other = lines[i].getAttribute("x2");
+            const y2Other = lines[i].getAttribute("y2");
+            const distanceStart = Math.sqrt((x - x1Other) ** 2 + (y - y1Other) ** 2);
+            const distanceEnd = Math.sqrt((x - x2Other) ** 2 + (y - y2Other) ** 2);
+            if (distanceStart <= 10) {
+                this.element.setAttribute("x1", x1Other);
+                this.element.setAttribute("y1", y1Other);
+            }
+            if (distanceEnd <= 10) {
+                this.element.setAttribute("x1", x2Other);
+                this.element.setAttribute("y1", y2Other);
+            }
+        }
+    }
+
+
+
+
+    addPoint(x, y) {
+      this.element.setAttribute("x2", x);
+      this.element.setAttribute("y2", y);
+      this.boardText.setAttribute('style', 'display: none;');
+      const lines = document.querySelectorAll('line');
+        for(let i=0; i<lines.length; i++){
+            const x1Other = lines[i].getAttribute("x1");
+            const y1Other = lines[i].getAttribute("y1");
+            const x2Other = lines[i].getAttribute("x2");
+            const y2Other = lines[i].getAttribute("y2");
+            const distanceStart = Math.sqrt((x - x1Other) ** 2 + (y - y1Other) ** 2);
+            const distanceEnd = Math.sqrt((x - x2Other) ** 2 + (y - y2Other) ** 2);
+            if (distanceStart <= 10) {
+                this.element.setAttribute("x2", x1Other);
+                this.element.setAttribute("y2", y1Other);
+            }
+            if (distanceEnd <= 10) {
+                this.element.setAttribute("x2", x2Other);
+                this.element.setAttribute("y2", y2Other);
+            }
+
+        }
+    }
+
+
+
+
+    remove() {
+      this.element.remove();
+    }
+  }
+
+  
+  const drawingBoard = new Draw('board');
